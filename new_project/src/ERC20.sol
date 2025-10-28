9192cb3bdd6ee4ca159c004433aa0cf65149a92b
NoahTesson <noah.tesson@epitech.eu>
2025-10-28 14:31:45 +0100

    [ADD] - variable

--- new_project/src/ERC20.sol
+++ new_project/src/ERC20.sol
@@ -4,5 +4,10 @@ pragma solidity ^0.8.20;
 import "./interface/IERC20.sol";
 
 contract MyToken is IERC20 {
+    string public name = "MyToken";
+    string public symbol = "MTK";  
+    uint8 public decimals = 18;
+    uint256 private _totalSupply;
+
 
 }
